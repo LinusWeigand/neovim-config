9ee3064c6b4cc504dc6110ce0cf098f6f2050d1c
Linus Weigand <linusweigand@protonmail.com>
2023-04-14 13:53:50 +0200

    treesitter add languages

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua" },
+    ensure_installed = { "terraform" },
+    ensure_installed = { "bash" },
+    ensure_installed = { "yaml" },
+    ensure_installed = { "rust" },
+    ensure_installed = { "python" },
+    ensure_installed = { "java" },
+    ensure_installed = { "javascript" },
+    ensure_installed = { "typescript" },
+    ensure_installed = { "latex" },
+    ensure_installed = { "ocaml" },
   },
 }
